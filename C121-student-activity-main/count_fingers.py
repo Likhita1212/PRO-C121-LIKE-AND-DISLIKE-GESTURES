d55b86bee101f281529b853c19aae40148a8552c
Likhita1212 <91164103+Likhita1212@users.noreply.github.com>
2022-12-22 19:56:27 -0500

    Add files via upload

---
+++ C121-student-activity-main/count_fingers.py
@@ -0,0 +1,31 @@
+import cv2
+import mediapipe as mp 
+wCam, hCam = 720,640
+cap = cv2.VideoCapture(0)
+
+cap.set(3,wCam)
+cap.set(4,hCam)
+mp_drawing = mp.solutions.drawing_utils 
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(min_dectection_confidence=0.8,min_tracking_confidence=0.5)
+
+def drawHandLandmarks(image_hand_landmarks):
+    if hand_landmarks:
+        for landmarks in hand_landmarks:
+            mp_drawing.draw_landmarks(image,landmarks,mp,mp_hands.HAND_CONNECTIONS)
+
+while True:
+    success, image = cap.read()
+    image = cv2.flip(image,1)
+    results = hands.process(image)
+    hand_landmarks = results.mulit_hand_landmarks
+    drawHandLandmarks(image,hand_landmarks)
+
+    cv2.imshow("Media Controller", image)
+
+    key = cv2.waitKey(1)
+    if key == 32:
+        break
+
+cv2.destroyAllWindows()
+
